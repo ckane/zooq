e7cb9c3cb45ae1cda684509a43a9dec6fcca01b5
Coleman Kane <ckane@colemankane.org>
2014-07-20 21:10:29 -0400

    Implement control pipe and control methods

--- zooq.py
+++ zooq.py
@@ -3,13 +3,17 @@
 
 #
 # Schema for pending queue objects:
-# {'task_obj': <obj id>, 'task_name': 'task name', 'pid': 999}
+# {'task_obj': <obj id>, 'task_name': 'task name', 'pid': 999, 'priority': 'low'}
 #
 
 from time import sleep
-from os import waitpid, WNOHANG, fork
-from ztasks import ztask_base
+from os import waitpid, WNOHANG, fork, pipe, fdopen, close, O_NONBLOCK
+from select import select
+from fcntl import fcntl, F_SETFL
+import json
 from ztasks import *
+import ztasks.ztask_base
+import sys
 
 class ZooQ(object):
     def __init__(self, max_procs=8, heartbeat=10):
@@ -18,6 +22,9 @@ class ZooQ(object):
         self.__active_queue = []
         self.__shutdown = False
         self.__heartbeat = heartbeat
+        self.__runq_pid = -1
+        self.__pwrite = False
+        self.__qread = False
 
     def qsize(self):
         return len(self.__pending_queue) + len(self.__active_queue)
@@ -25,7 +32,29 @@ class ZooQ(object):
     def waitsize(self):
         return len(self.__pending_queue)
 
+    def sendtask(self, task_name, objid, priority='low'):
+        self.__pwrite.write(json.dumps({'task_name': task_name, 'task_obj': objid, 'pid': -1, 'priority': priority}) + '\n')
+        self.__pwrite.flush()
+
+    def sendshutdown(self):
+        self.__pwrite.write('shutdown\n')
+        self.__pwrite.flush()
+
     def Run(self):
+        self.__qread, self.__pwrite = pipe()
+        self.__runq_pid = fork()
+
+        if self.__runq_pid != 0:
+            close(self.__qread)
+            self.__qread = False
+            self.__pwrite = fdopen(self.__pwrite, 'w')
+            return
+
+        sys.stdin.close()
+        close(self.__pwrite)
+        self.__pwrite = False
+        self.__qread = fdopen(self.__qread)
+        fcntl(self.__qread, F_SETFL, O_NONBLOCK)
         while not self.__shutdown:
             # Clean up any exited children
             try:
@@ -42,15 +71,29 @@ class ZooQ(object):
 
             # If workers are full, or no pending work to do, then just sleep
             if len(self.__active_queue) >= self.__max_procs or len(self.__pending_queue) == 0:
-                sleep(self.__heartbeat)
+                rs, ws, xs = select([self.__qread], [], [], self.__heartbeat)
+                if len(rs) > 0:
+                    job_request = self.__qread.readline()
+                    print("Request received: {0}".format(job_request.strip()))
+                    if(len(job_request) > 0):
+                        if job_request.strip().lower() == 'shutdown':
+                            self.__shutdown = True
+                        else:
+                            newtask = json.loads(job_request.strip())
+                            newtask['pid'] = -1
+                            if newtask['priority'] == 'high':
+                                self.__pending_queue.append(newtask)
+                            else:
+                                self.__pending_queue.insert(0, newtask)
             else:
                 while len(self.__active_queue) < self.__max_procs and len(self.__pending_queue) > 0:
                     # Attempt to migrate more tasks from the pending queue while there are pending tasks,
                     # and as long as there are available worker slots
                     nextjob = self.__pending_queue.pop()
-                    if nextjob['task_name'] in dir():
-                        task_instance = eval('{0}'.format(nextjob['task_name']))(nextjob['task_obj'])
-                        nextjob['pid'] = os.fork()
+                    print('Available: {0}'.format(json.dumps(dir(ztasks))))
+                    if nextjob['task_name'] in dir(ztasks):
+                        task_instance = eval('ztasks.{0}.{0}'.format(nextjob['task_name']))(nextjob['task_obj'])
+                        nextjob['pid'] = fork()
 
                         if nextjob['pid'] == 0:
                             # We are executing as the child
@@ -63,3 +106,4 @@ class ZooQ(object):
                         # In the event that the task spec referenced a non-existent task_name, display a
                         # friendly error, and discard it
                         print("Task {0} is not defined, discarding".format(nextjob['task_name']))
+
--- ztasks/ztask_base.py
+++ ztasks/ztask_base.py
@@ -6,7 +6,7 @@ class ztask_base(object):
     def __init__(self, objid):
         self.__objid = objid
 
-    def dowork():
+    def dowork(self):
         print "Doing work, waiting {0} seconds...".format(self.__objid)
         sleep(self.__objid)
         print "Done waiting {0} seconds...".format(self.__objid)
