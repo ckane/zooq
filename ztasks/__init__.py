3a1dbb4d88f166b07679e67fafa6ff3f0c0a0107
Coleman Kane <ckane@colemankane.org>
2014-07-21 00:06:04 -0400

    Add a demonstration of creating a new derived task and having it auto-discover correctly

--- ztasks/__init__.py
+++ ztasks/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 
 # List your additional task definitions here
-__all__ = []
+__all__ = ['derived_test']
---
+++ ztasks/derived_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+# This is an example task which will be automatically discovered
+# by the name of 'derived_test' by ZooQ, and will be plugged in
+# as a supported 'task_name'.
+#
+# Simply demonstrates inheritance as well as how the framework
+# and discovery works for new task definitions. In this case, it
+# is a modification which executes each task twice.
+#
+from ztask_base import ztask_base
+
+class derived_test(ztask_base):
+    def __init__(self, objid):
+        super(derived_test, self).__init__(objid)
+
+    def dowork(self):
+        # Execute underlying work action twice
+        super(derived_test, self).dowork()
+        super(derived_test, self).dowork()
