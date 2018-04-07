6594eee668a563d73237669b0dd05a84fbe1e5fb
Coleman Kane <ckane@colemankane.org>
2018-04-06 23:07:20 -0400

    Add args to main code, and make executable

---
+++ zooq_main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+#
+from zooq import ZooQ
+from zooqdb_sqlite import ZooQDB_SQLite
+import time
+from argparse import ArgumentParser
+
+ap = ArgumentParser(description="Example main service for a ZooQ")
+ap.add_argument('-d', '--dir', required=False, default='.', action='store',
+                help='Root folder for ZooQ')
+ap.add_argument('-n', '--numprocs', required=False, default=4, action='store',
+                type=int, help='Number of parallel processes')
+ap.add_argument('-f', '--foreground', required=False, default=False, action='store_true',
+                type=bool, help='Remain in foreground')
+args = ap.parse_args()
+
+db = ZooQDB_SQLite('{dir}/zooqdb.sqlite'.format(dir=args.dir))
+
+z = ZooQ(max_procs=args.numprocs, db=db)
+z.Run()
+
+while args.foreground and True:
+    time.sleep(1)
